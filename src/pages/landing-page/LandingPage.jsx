f1ee84a005d902958f2861920d4fc7a901f4fb06
khalid <khalidmamdou7@gmail.com>
2023-09-14 19:21:25 +0300

    update reg stat

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -7,8 +7,8 @@ function LandingPage() {
             <p>It's ugly, but it works. Not like the real one.</p>
             <button type="button" className='btn btn-primary' onClick={() => window.location.href = '/dry-run'}>Go to the Dry Run</button>
 
-            <p style={{ marginTop: '32px' }}>Based on the latest Registration Status Report Data: 18:00 14/9</p>
-            <p>Developed by <a href="https://www.linkedin.com/in/khalidmamdou7/">Khaled Mamdouh</a> and <a href="https://www.linkedin.com/in/omar-al-sharif/">Omar Al Sharif</a></p>
+            <p style={{ marginTop: '32px', color: 'grey'}}>Based on the latest Registration Status Report Data: 7:00 pm 14/9</p>
+            <p style={{ color: 'grey'}}>Developed by <a href="https://www.linkedin.com/in/khalidmamdou7/">Khaled Mamdouh</a> and <a href="https://www.linkedin.com/in/omar-al-sharif/">Omar Al Sharif</a></p>
         </div>
     );
 };
