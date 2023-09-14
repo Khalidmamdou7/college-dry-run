3f17841fec62eebe3e297fc8019e617f10a4f5e6
khalid <khalidmamdou7@gmail.com>
2023-09-14 19:51:55 +0300

    add contribution message

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -7,8 +7,13 @@ function LandingPage() {
             <p>It's ugly, but it works. Not like the real one.</p>
             <button type="button" className='btn btn-primary' onClick={() => window.location.href = '/dry-run'}>Go to the Dry Run</button>
 
-            <p style={{ marginTop: '32px', color: 'grey'}}>Based on the latest Registration Status Report Data: 7:30 pm 14/9</p>
-            <p style={{ color: 'grey'}}>Developed by <a href="https://www.linkedin.com/in/khalidmamdou7/">Khaled Mamdouh</a> and <a href="https://www.linkedin.com/in/omar-al-sharif/">Omar Al Sharif</a></p>
+            <div className={styles['landing-page-footer']}>
+
+                <p>Based on the latest Registration Status Report Data: 7:30 pm 14/9</p>
+                <p>Developed by <a href="https://www.linkedin.com/in/khalidmamdou7/">Khaled Mamdouh</a> and <a href="https://www.linkedin.com/in/omar-al-sharif/">Omar Al Sharif</a></p>
+                <p>Feel free to contribute to the <a href="https://github.com/Khalidmamdou7/college-dry-run">Github Repository</a></p>
+            </div>
+
         </div>
     );
 };
