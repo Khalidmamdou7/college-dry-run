ce0f002f0d4abf98f5b9426361087657936b3cab
eng.omar.al.sharif <eng.omar.al.sharif@gmail.com>
2023-12-22 19:24:56 +0200

    Spring 24 update

--- src/pages/dry-run/DryRun.jsx
+++ src/pages/dry-run/DryRun.jsx
@@ -2,6 +2,7 @@ import styles from './DryRun.module.css';
 import img from './registration.png';
 import { useState } from 'react';
 
+
 import TimeTable from './time-table/TimeTable';
 import CourseSelector from './course-selector/CourseSelector';
 
@@ -18,23 +19,30 @@ function DryRun() {
 
     return (
         <div className={styles['page-container']}>
-            <div className={styles['page-header']} style={{ display: 'flex', alignItems: 'center', marginBottom: '16px'}}>
-                <img src={img} alt =" " style={{marginLeft: '300px'}} />
-                <h1 style={{marginLeft: '30px'}}> Dry Run تحت السلم</h1>
+            <div className={styles['page-header']} style={{ display: 'flex', alignItems: 'center', marginBottom: '16px' }}>
+                <img src={img} alt=" " style={{ marginLeft: '300px' }} />
+                <h1 style={{ marginLeft: '30px' }}> Dry Run تحت السلم</h1>
             </div>
             <div className={styles['page-content']} >
-                <div className={styles['dry-run-container']} style={{ backgroundColor: 'white'}}>
+                <div className={styles['dry-run-container']} style={{ backgroundColor: 'white' }}>
                     <div className={styles['time-table-container']} >
                         <TimeTable timeslots={timeslots} />
                     </div>
-                    <p>Based on the data sent on pdfs at 21/12/2023</p>
-                    <p>Special thanks to Yousif Ismail for collecting the data</p>
+                    {/* <p>Based on the data sent on pdfs at 21/12/2023</p>
+                    <p>Special thanks to Yousif Ismail for collecting the data</p> */}
+                    <div className={styles['landing-page-footer']}>
+
+                        <p>Based on registration status report 22/12/2023 7:15 PM</p>
+                        {/* <p>Special thanks to Yousif Ismail for collecting the data</p> */}
+                        <p>Developed by <a href="https://www.linkedin.com/in/khalidmamdou7/" target='_blank' rel="noreferrer">Khaled Mamdouh</a> and <a href="https://www.instagram.com/o.al.sharif/" target='_blank' rel="noreferrer">Omar Al Sharif</a></p>
+                        <p>Feel free to contribute to the <a href="https://github.com/Khalidmamdou7/college-dry-run">Github Repository</a></p>
+                    </div>
                 </div>
                 <div className={styles['courses-selector-container']}>
                     <CourseSelector handleCoursesChange={handleCoursesChange} />
                 </div>
             </div>
-            
+
         </div>
     );
 }
--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -3,15 +3,16 @@ import styles from './LandingPage.module.css';
 function LandingPage() {
     return (
         <div className={styles['landing-page-container']}>
-            <h1>Here is the ugliest College Schedule Maker (CUFE CHS Registration)</h1>
+            {/* <h1>Welcome to the ugliest College Schedule Maker (CUFE CHS Registration)</h1> */}
+            <h1>Welcome to the Dry Run - تحت السلم  (CUFE CHS College Schedule Maker)</h1>
             <p>It's ugly, but it works. Not like the real one.</p>
             <button type="button" className='btn btn-primary' onClick={() => window.location.href = '/dry-run'}>Go to the Dry Run</button>
 
             <div className={styles['landing-page-footer']}>
 
-                <p>Based on the data sent on pdfs at 21/12/2023</p>
-                <p>Special thanks to Yousif Ismail for collecting the data</p>
-                <p>Developed by <a href="https://www.linkedin.com/in/khalidmamdou7/">Khaled Mamdouh</a> and <a href="https://www.linkedin.com/in/omar-al-sharif/">Omar Al Sharif</a></p>
+                <p>Based on registration status report 22/12/2023 7:15 PM</p>
+                {/* <p>Special thanks to Yousif Ismail for collecting the data</p> */}
+                <p>Developed by <a href="https://www.linkedin.com/in/khalidmamdou7/" target='_blank' rel="noreferrer">Khaled Mamdouh</a> and <a href="https://www.instagram.com/o.al.sharif/" target='_blank' rel="noreferrer">Omar Al Sharif</a></p>
                 <p>Feel free to contribute to the <a href="https://github.com/Khalidmamdou7/college-dry-run">Github Repository</a></p>
             </div>
 
